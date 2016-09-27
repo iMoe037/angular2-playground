4158c713e064143ddd2d25dedec13f313437f7e6
Immad Mohamed <i.mohamed037@gmail.com>
2016-09-26 23:57:57 -0400

    Redo for official version of Angualr 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,7 @@
-import { Component } from "@angular/core";
-import { ROUTER_DIRECTIVES } from "@angular/router";
-
-import { HeroService } from "./hero.service";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "my-playground",
-  template: `
-  <h1>{{title}}</h1>
-  <nav>
-    <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
-    <a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
-  </nav>
-  <router-outlet></router-outlet>
-`,
-
-  directives: [ROUTER_DIRECTIVES],
-  providers: [HeroService]
+    selector: 'my-app',
+    template: '<h1>My First Angular App</h1>'
 })
-
-export class AppComponent {
-  title = "Tour of Heroes";
-}
+export class AppComponent { }
--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,5 @@
-import { bootstrap }    from "@angular/platform-browser-dynamic";
-import { AppComponent } from "./app.component";
-import { appRouterProviders } from "./app.routes";
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-bootstrap(AppComponent, [
-  appRouterProviders
-]);
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
